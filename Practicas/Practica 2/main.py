9e71b95fd0851033bb6ea8e97fb47b9a659db064
Nacxit <86132894+NacxitCotuha@users.noreply.github.com>
2022-03-23 23:44:34 -0600

    a

--- Practicas/Practica 2/main.py
+++ Practicas/Practica 2/main.py
@@ -12,6 +12,10 @@ def main() -> None:
             host_ip: str = sys.argv[0]
             puerto: int = int(sys.argv[1])
             comunidad: str = sys.argv[2]
+            if " " in host_ip:
+                errorMessage()
+            if " " in comunidad:
+                errorMessage()
             menu(host_ip=ip, puerto=puerto)
             sys.exit(0)
         else:
@@ -38,12 +42,24 @@ def menu( host_ip: str, puerto: int ) -> None:
             print("Opcion no valida")
 
 
-def verificarConexion( host_ip: str, puerto: int ) -> bool:
+def verificarConexion( host_ip: str, puerto: int, comunidad: str) -> bool:
     contador: int = 0
+    estado: bool
     while contador < 5:
-        estado = FuncionesRRD.consultaSNMP(comunidad=)
+        is_up = FuncionesRRD.consultaSNMP(comunidad=comunidad, host=host_ip, oid="1.3.6.1.2.1.1.3.0")
+        if is_up:
+            estado = True
+            break
+        estado = False
+        contador = contador + 1
         time.sleep(5)
+    else:
+        if contador >= 5:
+            estado = False
+        else:
+            estado = True
     clearConsole()
+    return estado
 
 
 def clearConsole() -> None:
@@ -56,7 +72,8 @@ def clearConsole() -> None:
 
 def errorMessage() -> None:
     print("La practica se debe compilar de la siguiente manera: ")
-    print("$ python3 main.py <ip:http_service> <port:numero_int>")
+    print("$ python3 main.py <ip:http_service> <port:numero_int> <comunidad:nombre>")
+    print("La ip y la comunidad deben ser escritas sin espacios entre si")
     sys.exit(-1)
 
 
